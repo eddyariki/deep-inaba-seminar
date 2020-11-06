ac1ce3f71e85624d5bc4811ffde7ee91801267b5
松浦瑠希 <matsuura@technosolution.co.jp>
2020-11-06 22:21:16 +0900

    [add] easy data

---
+++ dataset/easy_data.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def load_easy_data(size = 500, dim = 1):
+    X = []
+    for i in range(dim):
+        X.append(np.random.rand(size))
+    X = np.array(X).T
+    
+    y = np.sum(X, axis = 1) / np.sqrt(dim)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+
+    return X_train, X_test, y_train, y_test
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = load_easy_data()
+    print(X_train[:5])
+    print(X_test[:5])
+    print(y_train[:5])
+    print(y_test[:5])
